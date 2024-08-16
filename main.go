27d618aa9384947b93c6c4470f5fbe377c22458c
miskam-gg <143910099+miskam-gg@users.noreply.github.com>
2024-08-16 19:23:51 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,24 +9,20 @@ import (
 )
 
 func main() {
-	// Получаем значения переменных окружения
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	dbname := os.Getenv("DB_NAME")
 
-	// Конструкция строки подключения
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable",
 		host, port, user, dbname)
 
-	// Открытие соединения с базой данных
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	// Проверка соединения
 	err = db.Ping()
 	if err != nil {
 		panic(err)
@@ -34,6 +30,5 @@ func main() {
 
 	fmt.Println("Successfully connected to the database!")
 
-	// Вызов функции для настройки маршрутов
 	setupRoutes(db)
 }
